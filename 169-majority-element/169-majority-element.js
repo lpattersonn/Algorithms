69cb529cb36558d3b2ec5388aa634ca1f5c7834d
Leshan Patterson <86181162+lpattersonn@users.noreply.github.com>
2022-06-05 22:40:55 -0400

    Time: 85 ms (61.16%), Space: 45.3 MB (28.65%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -5,13 +5,17 @@
 var majorityElement = function(nums) {
     let obj = {};
     let count = 0;
+    
+    // loop through nums
     for (let num of nums) {
+        // convert values as am object
         if(obj[num] === undefined) {
             obj[num] = 1
         } else {
             obj[num]++
         }
     }
+    // loop through objects return key that has a value greater than n/2
     for (let keys in obj) {
         if (obj[keys] > nums.length / 2) {
             count = Number(keys)
@@ -20,7 +24,3 @@ var majorityElement = function(nums) {
     
     return count;
 };
-
-// loop through nums
-// convert values as am object
-// loop through object of values === n/2
