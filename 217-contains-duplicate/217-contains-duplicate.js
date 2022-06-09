cde3e83617c12e32a8eb34ea4c2ef7267050486f
Leshan Patterson <86181162+lpattersonn@users.noreply.github.com>
2022-06-09 14:27:38 -0400

    Time: 342 ms (14.77%), Space: 68.6 MB (5.18%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let obj = {}; 
+    let result = false;
+    
+    for (let num of nums) {
+        if (obj[num] === undefined) {
+            obj[num] = 1;
+        } else {
+            obj[num]++;
+        }
+    }
+    
+    for (let keys in obj) {
+        if (obj[keys] > 1)
+            result = true;
+    }
+    console.log(obj, result)
+    return result;
+};
