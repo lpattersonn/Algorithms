6518059a254ea132f2605d937084986e9a470a01
Leshan Patterson <86181162+lpattersonn@users.noreply.github.com>
2022-07-30 18:01:51 -0400

    Time: 133 ms (15.46%), Space: 45.5 MB (19.24%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    let sObj = {};
+    let tObj = {};
+    let result = "";
+    for (let ele of s) {
+        if (sObj[ele] === undefined) {
+            sObj[ele] = 1;
+        } else {
+            sObj[ele]++;
+        }
+    }
+     for (let ele of t) {
+        if (tObj[ele] === undefined) {
+            tObj[ele] = 1;
+        } else {
+            tObj[ele]++;
+        }
+    }
+    
+    for (let key in tObj) {
+        if (sObj[key] !== tObj[key]) {
+            result = key;
+        }
+    }
+    console.log(sObj, tObj);
+    return result;
+};
