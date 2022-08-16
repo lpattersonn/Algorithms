13e48149f48a428f43b161174a029dab8d1e15d0
Leshan Patterson <86181162+lpattersonn@users.noreply.github.com>
2022-08-15 22:30:14 -0400

    Time: 108 ms (35.87%), Space: 42.1 MB (75.78%) - LeetHub

---
+++ 520-detect-capital/520-detect-capital.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) {
+    if (word === word.toUpperCase()) {
+        return true;
+    } else if (word === word.toLowerCase()) {
+        return true;
+    } else if (word === word[0].toUpperCase() + word.slice(1).toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
+    
+};
